99b6de9dc75dc3d45e9a47e66967e843cf5e3dbc
Sapronov-Nikolay <avindialit@list.ru>
2023-11-21 00:07:18 +0300

    Произведено создание модели для записей.

--- rocket/delo/models.py
+++ rocket/delo/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone 
+# После импорта модуля timezone мы можем использовать параметр timezone.now в качестве аргумента 
+# по умолчанию для date_created. 
+# Теперь при создании новой записи не придётся вручную указывать время и дату. 
+# Это пригодится нам позже
 
-# Create your models here.
+class Delo(models.Model): # Создаём новый класс, который будет служить для блога моделью, указывая все необходимые элементы.
+    title = models.CharField(max_length=100)
+    content = models. TextFild()
+    date_created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self): # С помощью функции меняем то, как будет представлена запись в модели.
+        return self.title # Указываем, что она будет идентифицироваться с помощью своего заголовка.
+    
+    class Meta:
+        verbos_name_plural = "Dels" # Указываем правильное написание для множественного числа слова Delo.
