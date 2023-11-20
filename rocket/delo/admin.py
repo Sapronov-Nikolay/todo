5784d02c8ba024515207c51fca3c8e4313c24d2c
Sapronov-Nikolay <avindialit@list.ru>
2023-11-21 00:08:42 +0300

    Регистрация созданной модели в 'models.py'

--- rocket/delo/admin.py
+++ rocket/delo/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Delo # Импортируем модель в админке приложения
+
+admin.site.register(Delo) # Регистрируем модель.
 
 # Register your models here.
