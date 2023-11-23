637e22fa7e999615643f64c39ab7592070ddb784
Sapronov-Nikolay <avindialit@list.ru>
2023-11-23 06:35:09 +0300

    Проедена связь и регистрация приложения tasklist

--- rocket/rocket/settings.py
+++ rocket/rocket/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 	'mainpage',
     'delo',
+    'tasklist',
 ]
 
 MIDDLEWARE = [
--- rocket/rocket/urls.py
+++ rocket/rocket/urls.py
@@ -17,10 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from mainpage import views
+from django.urls import path, include
+
 
 urlpatterns = [
 	path('' , views.index),
     path('admin/', admin.site.urls),
+    path('tasklist/', include('tasklist.urls')), 
 ]
 
 navset = [
