fb1bee80a8e56ba0ba2669d487b5a9f1727d6169
Sapronov-Nikolay <avindialit@list.ru>
2023-11-28 00:42:26 +0300

    Подджиэсим и подсиэсэсим нашу табличку

---
+++ rocket/static/script/save_tasks.js
@@ -0,0 +1,7 @@
+function create_task(answers) {
+    /* answers - ответы в форме, т.е. в input-ах*/
+	console.log(answers.start.value);
+	console.log(answers.end.value);
+	console.log(answers.description.value);
+	console.log(answers.done.checked);
+}
